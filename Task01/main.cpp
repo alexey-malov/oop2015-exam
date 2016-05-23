d7de9784e2c133ae36f44cbee1ac8c9f41184cfe
Alexey Malov <alexey.malov@cpslabs.net>
2016-05-24 01:25:07 +0300

    Добавлены 4 задания

---
+++ Task01/main.cpp
@@ -0,0 +1,61 @@
+﻿#include <vector>
+#include <memory>
+
+using namespace std;
+
+// Холст, позволяющий рисовать графические примитивы
+class ICanvas
+{
+public:
+	virtual void DrawLine(int x0, int y0, int x1, int y1) = 0;
+	virtual ~ICanvas() {}
+};
+
+// Реализация холста, выполняющая вывод информации о рисуемых примитивах в stdout в виде:
+// DrawLine:(<x0>, <y0>) - (<x1>, <y1>)
+class CCanvas : public ICanvas
+{
+	// Написать недостающий код
+};
+
+// Интерфейс объектов, которые можно нарисовать на холсте
+class ICanvasDrawable
+{
+	// Написать недостающий код
+};
+
+// Прямоугольник, обладающий координатами верхнего левого угла, шириной и высотой
+class CRectangle : public ICanvasDrawable
+{
+	// Написать недостающий код
+};
+
+// Треугольник, задается координатами трех вершин
+class CTriangle : public ICanvasDrawable
+{
+	// Написать недостающий код
+};
+
+// Рисует картину picture на холсте canvas
+void DrawPicture(ICanvas & canvas, const vector<shared_ptr<const ICanvasDrawable>> & picture)
+{
+	// Написать недостающий код
+}
+
+int main()
+{
+	/*
+	Задание:
+		При помощи имеющихся фигур описать домик
+		"Нарисовать" его при помощи функции DrawPicture на "холсте"
+	   /\
+	  /  \
+	 /    \
+	--------
+	 |    |
+	 |    |
+	 |    |
+	 ------
+	*/
+	return 0;
+}
---
+++ Task02/main.cpp
@@ -0,0 +1,69 @@
+﻿#include <string>
+
+using namespace std;
+
+// *Односвязный* список строк
+class CStringList
+{
+public:
+	CStringList()
+	{
+		// Написать недостающий код
+	}
+
+	CStringList(const CStringList & list)
+	{
+		// написать недостающий код
+	}
+
+	~CStringList()
+	{
+		// Написать недостающий код
+	}
+
+	CStringList& operator=(const CStringList &list)
+	{
+		// написать недостающий код
+	}
+
+	// Добавляет строку в начало списка. Время выполнения: O(1)
+	void AddToBegin(const string& value)
+	{
+		// Написать недостающий код
+	}
+
+	// Возвращает размер списка. Время выполнения: O(1)
+	size_t GetSize()const
+	{
+		// Написать недостающий код
+	}
+
+	// Возвращает первый элемент списка. Бросает logic_error, если список пуст
+	// Время выполнения: O(1)
+	string GetFront()const
+	{
+		// Написать недостающий код
+	}
+
+	// Выводит элементы списка в out (по одному в каждой строке)
+	void PrintList(ostream & out)const
+	{
+		// Написать недостающий код
+	}
+
+	// Очищает список
+	void Clear()
+	{
+		// Написать недостающий код
+	}
+
+private:
+	// Написать недостающий код
+};
+
+
+
+int main()
+{
+	return 0;
+}
